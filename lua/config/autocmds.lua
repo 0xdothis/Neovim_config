4cec60805f1bc3d1501fe1aedde2bde6eba3fefb
joshdfg <slimmy0310@gmail.com>
2025-04-10 17:42:10 +0100

    feat: added new config

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -11,14 +11,14 @@ local lsp_fmt_group = vim.api.nvim_create_augroup("LspFormattingGroup", {})
 vim.api.nvim_create_autocmd("BufWritePre", {
   group = lsp_fmt_group,
   callback = function()
-      local efm = vim.lsp.get_clients({ name = "efm" })
+    local efm = vim.lsp.get_clients({ name = "efm" })
 
     if vim.tbl_isempty(efm) then
       return
-      end
+    end
 
     vim.lsp.buf.format({ name = "efm", async = true })
-   end,
+  end,
 })
 
 -- highlight on yank
@@ -27,15 +27,15 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   group = highlight_yank_group,
   callback = function()
     vim.highlight.on_yank()
-   end,
+  end,
 })
 -- Autocommand for Solidity files
 vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = "*.sol",
-    callback = function()
-        vim.cmd("silent! !forge fmt %") -- Run forge fmt
-        vim.cmd("e!")                   -- Reload the file to reflect changes
-    end,
+  pattern = "*.sol",
+  callback = function()
+    vim.cmd("silent! !forge fmt %") -- Run forge fmt
+    vim.cmd("e!") -- Reload the file to reflect changes
+  end,
 })
 
 -- Autocommand for Rust files
@@ -45,3 +45,4 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 --         vim.lsp.buf.format({ async = false })
 --     end,
 -- })
+
